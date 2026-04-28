4aace2bc0a26b876fcae44ad35fc14b288d2c10a
devgabrielsborges <dev.gabrielsborges@proton.me>
2026-04-27 21:54:54 -0300

    feat(compression)/fine-tune model with compressed tensors

--- src/data/compression.py
+++ src/data/compression.py
@@ -30,6 +30,10 @@ def build_tensor_transform() -> transforms.Compose:
     )
 
 
+def build_normalize_transform() -> transforms.Normalize:
+    return transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+
 def preprocess_pil_image(
     image: Image.Image,
     image_size: int = 512,
